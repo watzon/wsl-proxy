e28b60486c1ca24dd2f786135572665f119ed6d9
flowstatedev <flowstatedev@gmail.com>
2019-06-09 08:20:59 -0400

    Add scripts to automatically collect/generate proxies

---
+++ gen-proxies.cmd
@@ -0,0 +1,16 @@
+@echo off
+pushd %~dp0
+
+rem Unfortunately the following won't work because
+rem it runs into a length limit in CMD and only processes
+rem about half of /usr/bin
+rem
+rem mkdir autogen
+rem for /f %%x in ('wsl ls /usr/bin') do echo @echo off ^
+rem
+rem bash.exe -c "%%x %%*" > autogen/%xx.bat
+
+rem So we'll use a bash script instead
+bash -c ./gen-proxies.sh
+
+popd
